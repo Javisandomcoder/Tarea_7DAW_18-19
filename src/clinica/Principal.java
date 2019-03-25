c2b733395fd982cc1905641a992d0c32b381125d
Javisandom <javisandom@gmail.com>
2019-03-25 01:09:02 +0100

    Buscando utilidad del Enumerado

--- src/clinica/Principal.java
+++ src/clinica/Principal.java
@@ -6,7 +6,6 @@
 package clinica;
 
 import java.io.File;
-import java.io.Serializable;
 import utilidades.ES;
 
 /**
@@ -21,6 +20,7 @@ public class Principal  {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int opcion;
         String nombreFichero = "pacientes.dat";
         File fichero = new File(nombreFichero);
         Paciente[] paciente = new Paciente[MAX_PACIENTES];//Array de pacientes
@@ -30,6 +30,8 @@ public class Principal  {
         } else {
             System.out.println("NO EXISTE FICHERO");
         }
+        
+        opcion = ES.leeEntero("Introduzca la opción elegida", 0, 6);
 
         //Presentamos el menú
         do {
@@ -44,7 +46,36 @@ public class Principal  {
             ES.msgln("\t\t 6.- Escribir lista de pacientes a txt.\n");
             ES.msgln("\t\t 0.- Salir de la aplicación.");
             ES.msgln("\t =============================================");
-        } while (false);
+        } while (opcion != 0);
+
+    }
+    
+    private static boolean insertarPaciente(Paciente[] listado) {
+        int posicionEnArray = 0;
+        boolean colocar = true;
+        boolean insertado = false;
+        Paciente paciente;
+
+        if (listado[listado.length - 1] != null) {
+            ES.msgln("Error: Almacén lleno");
+        } else {//Pedimos los datos de la caja
+            ES.msgln("Introduzca los datos del paciente:");
+            String NIF = ES.leeCadena("Escriba el NIF del paciente:");
+            String nombrePaciente = ES.leeCadena("EScriba el nombre del paciente");
+            String emailNotificaciones = ES.leeCadena("Escriba el email del paciente");
+            int tipoPaciente = ES.leeEntero("Escriba el tipo de paciente (1-> PRIVADO, 2 -> MUTUALISTA)", 1, 2);
+            while (colocar && (posicionEnArray < Principal.MAX_PACIENTES)) {
+                if (listado[posicionEnArray] == null) {
+                    listado[posicionEnArray] = paciente;
+                    colocar = false;
+                    System.out.println("Se ha creado una nueva caja");
+                    insertado = true;
+                }
+                posicionEnArray++;
+            }
+
+        }
+        return insertado;
 
     }
 }
